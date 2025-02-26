9691d42eb98815dc5bd769a9cba84e000dcc4ceb
Julia Fokanova <oncologmma@mail.ru>
2025-02-27 02:21:13 +0300

    refactor: Footer

---
+++ src/components/Copyrights/Copyrights.tsx
@@ -0,0 +1,16 @@
+import './copyrights.css';
+
+const Copyrights = () => {
+  return (
+    <a
+      className="copyrights"
+      href="https://github.com/Professor-Severus-Snape"
+      target="_blank"
+      title="github"
+    >
+      2025 Julia Fokanova
+    </a>
+  );
+};
+
+export default Copyrights;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
+import Contacts from '../Contacts/Contacts';
+import Copyrights from '../Copyrights/Copyrights';
 import Logo from '../Logo/Logo';
+import Socials from '../Socials/Socials';
 import SubscribeForm from '../SubscribeForm/SubscribeForm';
 import './footer.css';
 
@@ -31,102 +34,31 @@ const Footer = () => {
         <div className="footer__top-container">
           <div className="footer__contacts">
             <h4 className="footer__title">Свяжитесь с нами</h4>
-            <ul className="footer__contact-list">
-              <li className="footer__contact-item">
-                <a
-                  href="tel:8 (800) 000 00 00"
-                  className="footer__contact-link phone"
-                ></a>
-                <span className="footer__contact">8 (800) 000 00 00</span>
-              </li>
-              <li className="footer__contact-item">
-                <a
-                  href="mailto:inbox@mail.ru"
-                  className="footer__contact-link mail"
-                ></a>
-                <span className="footer__contact">inbox@mail.ru</span>
-              </li>
-              <li className="footer__contact-item">
-                <a
-                  href="skype:tu.train.tickets?call"
-                  className="footer__contact-link skype"
-                ></a>
-                <span className="footer__contact">tu.train.tickets</span>
-              </li>
-              <li className="footer__contact-item">
-                <a
-                  href="https://yandex.ru/maps/?text=Москва, Московская 27-35 555 555"
-                  target="_blank"
-                  className="footer__contact-link map"
-                ></a>
-                <span className="footer__contact">
-                  г. Москва
-                  <br />
-                  ул. Московская 27-35
-                  <br />
-                  555 555
-                </span>
-              </li>
-            </ul>
+            <Contacts />
           </div>
+
           <div className="footer__subscribe">
-            <h4 className="footer__title">Подписка</h4>
-            <p className="footer__text">Будьте в курсе событий</p>
-            <SubscribeForm />
-            <h4 className="footer__title">Подписывайтесь на нас</h4>
-            <ul className="footer__subscribe-list">
-              <li className="footer__subscribe-item">
-                <a
-                  href="https://www.youtube.com"
-                  target="_blank"
-                  className="footer__subscribe-link youtube"
-                ></a>
-              </li>
-              <li className="footer__subscribe-item">
-                <a
-                  href="https://www.linkedin.com"
-                  target="_blank"
-                  className="footer__subscribe-link linkedin"
-                ></a>
-              </li>
-              <li className="footer__subscribe-item">
-                <a
-                  href="https://accounts.google.com/"
-                  target="_blank"
-                  className="footer__subscribe-link google"
-                ></a>
-              </li>
-              <li className="footer__subscribe-item">
-                <a
-                  href="http://facebook.com/"
-                  target="_blank"
-                  className="footer__subscribe-link facebook"
-                ></a>
-              </li>
-              <li className="footer__subscribe-item">
-                <a
-                  href="https://twitter.com"
-                  target="_blank"
-                  className="footer__subscribe-link twitter"
-                ></a>
-              </li>
-            </ul>
+            <div className="footer__subscribe-form-container">
+              <h4 className="footer__title">Подписка</h4>
+              <p className="footer__text">Будьте в курсе событий</p>
+              <SubscribeForm />
+            </div>
+            <div className="footer__socials">
+              <h4 className="footer__title">Подписывайтесь на нас</h4>
+              <Socials />
+            </div>
           </div>
         </div>
       )}
+
       <div className="footer__bottom-container" ref={refElement}>
         <Logo />
         <span
           className={isCollapsed ? 'footer__down' : 'footer__up'}
+          title={isCollapsed ? 'развернуть' : 'свернуть'}
           onClick={handleCollapse}
         ></span>
-        <a
-          href="https://github.com/Professor-Severus-Snape"
-          target="_blank"
-          className="footer__copyrights"
-        >
-          2025 Julia Fokanova
-        </a>
+        <Copyrights />
       </div>
     </footer>
   );
