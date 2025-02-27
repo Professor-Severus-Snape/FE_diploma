83fb5dc781dc6cae7289128b69f46f77d440ca49
Julia Fokanova <oncologmma@mail.ru>
2025-02-27 14:41:07 +0300

    refactor: SectionDescription

--- src/components/SectionDescription/SectionDescription.tsx
+++ src/components/SectionDescription/SectionDescription.tsx
@@ -1,32 +1,33 @@
+import { Link } from 'react-router-dom';
 import computer from '../../assets/computer.svg';
 import earth from '../../assets/earth.svg';
 import office from '../../assets/office.svg';
-
 import './sectionDescription.css';
 
 const SectionDescription = () => {
+  const items = [
+    { alt: 'Удобно', src: computer, text: 'Удобный заказ на сайте' },
+    { alt: 'Удаленно', src: office, text: 'Нет необходимости ехать в офис' },
+    { alt: 'Выбор', src: earth, text: 'Огромный выбор направлений' },
+  ];
+
   return (
     <section id="description" className="description">
-      <div className="description__top-container">
+      <header className="description__header">
         <h2 className="description__title">Как это работает</h2>
-        <a href="#footer" className="desription__link">Узнать больше</a>
-      </div>
-      <div className="description__bottom-container">
-        <div className="description__item">
-          <img src={computer} alt="Удобно" className="description__img" />
-          <p className="description__text">Удобный заказ на сайте</p>
-        </div>
+        <Link to="#footer" className="desription__link">
+          Узнать больше
+        </Link>
+      </header>
 
-        <div className="description__item">
-          <img src={office} alt="Удаленно" className="description__img" />
-          <p className="description__text">Нет необходимости ехать в офис</p>
-        </div>
-
-        <div className="description__item">
-          <img src={earth} alt="Много" className="description__img" />
-          <p className="description__text">Огромный выбор направлений</p>
-        </div>
-      </div>
+      <ul className="description__list">
+        {items.map((item, idx) => (
+          <li className="description__item" key={idx}>
+            <img src={item.src} alt={item.alt} className="description__img" />
+            <p className="description__text">{item.text}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
